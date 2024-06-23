2fbda4ca24ddabce88075d845649f6e523c3995b
abhiroopc84 <abhiroopc84@icloud.com>
2024-06-24 01:27:33 +0530

    Add about and icons

---
+++ src/app/about/page.tsx
@@ -0,0 +1,66 @@
+import skills from "@/constants/skills";
+import Image from "next/image";
+import Link from "next/link";
+
+const About = () => {
+  
+  return (
+    <div className="p-2 mt-8 flex flex-col">
+      <div>
+        <p className="w-2/5">
+          <span>my name is abhiroop reddy.</span>
+          <br />
+          <span>
+            a fourth year undergraduate student pursuing bachelor of engineering
+            in computer science.
+          </span>
+          <br />
+          <span>a front-end web developer.</span>
+          <br />
+          <br />
+          <span>based in hyderabad, telangana, india.</span>
+        </p>
+      </div>
+      <div className="mt-12 flex flex-col gap-2">
+        <span>skills</span>
+        <div className="flex flex-row gap-4">
+          {skills.map((skill) => (
+            <div className="flex flex-row items-center gap-2" key={skill.name}>
+              <span>{skill.name}</span>
+              <div>
+                <Image
+                  className="dark:invert"
+                  src={skill.icon}
+                  width={20}
+                  height={20}
+                  alt={`${skill.name} icon`}
+                ></Image>
+              </div>
+              <span>/</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-24">
+        <p className="w-2/5">
+          <span>
+            this site was built using next.js and shadcn/ui components.
+          </span>
+          <br />
+          <span>
+            heavily inspired by the{" "}
+            <Link
+              href="https://shadcn.com/"
+              className="underline underline-offset-4"
+            >
+              shadcn portfolio
+            </Link>
+            .
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/constants/skills.ts
@@ -0,0 +1,24 @@
+const skills = [
+  {
+    name: "react",
+    icon: "/react-icon.png",
+  },
+  {
+    name: "node.js",
+    icon: "/nodejs-icon.png",
+  },
+  {
+    name: "next.js",
+    icon: "/nextjs-icon.png",
+  },
+  {
+    name: "remix",
+    icon: "/remix-icon.png",
+  },
+  {
+    name: "django",
+    icon: "/django-icon.png",
+  },
+];
+
+export default skills;
