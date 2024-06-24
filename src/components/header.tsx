0efcf7196fecd48a318511f2e785c7a18179e49d
abhiroopc84 <abhiroopc84@icloud.com>
2024-06-24 14:50:42 +0530

    Add mobile icons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 import pages from "@/constants/pages";
 import socials from "@/constants/socials";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -16,7 +17,7 @@ const Header = () => {
         <div className="flex flex-col">
           {pages.map((page) => (
             <div className="flex gap-2" key={page.name}>
-              /
+              <span>/</span>
               <Link
                 href={page.link}
                 className={`link ${
@@ -29,17 +30,35 @@ const Header = () => {
           ))}
         </div>
       </div>
-      <div className="flex gap-2">
+      <div className="flex flex-col md:flex-row md:gap-2">
         {socials.map((social, index) => (
-          <div key={social.name} className="flex gap-2">
-            <a
-              href={social.link}
-              target="_blank"
-              className="underline underline-offset-4"
-            >
-              {social.name}
-            </a>
-            {index !== socials.length-1 && <span>/</span>}
+          <div key={social.name}>
+            <div className="hidden md:flex gap-2">
+              <a
+                href={social.link}
+                target="_blank"
+                className="underline underline-offset-4"
+              >
+                {social.name}
+              </a>
+              {index !== socials.length - 1 && <span>/</span>}
+            </div>
+            <div className="flex md:hidden gap-2">
+              <span>/</span>
+              <a
+                href={social.link}
+                target="_blank"
+                className="underline underline-offset-4"
+              >
+                <Image
+                  className="dark:invert"
+                  src={social.icon}
+                  alt={`${social.name} icon`}
+                  height={20}
+                  width={20}
+                />
+              </a>
+            </div>
           </div>
         ))}
       </div>
