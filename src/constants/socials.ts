1754c20b967b4c01f8686fddc97c1182c2aaaa85
abhiroopc84 <abhiroopc84@icloud.com>
2024-06-24 14:55:33 +0530

    Add social icons

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -2,14 +2,17 @@ const socials = [
   {
     name: "github",
     link: "https://github.com/abhiroopc84",
+    icon: "/github-icon.png"
   },
   {
     name: "twitter",
     link: "https://x.com/me_abhiroop",
+    icon: "/x-icon.png"
   },
   {
     name: "linkedin",
     link: "https://www.linkedin.com/in/abhiroopreddyc/",
+    icon: "/linkedin-icon.png"
   },
 ];
 
