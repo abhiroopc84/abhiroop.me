310e7dc8ff29b6ea7ca2e7b04b339444d0da90cf
abhiroopc84 <abhiroopc84@icloud.com>
2024-09-21 17:32:54 +0530

    Change layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,33 @@
-import Header from "@/components/header";
-import { ModeToggle } from "@/components/mode-toggle";
-import { ThemeProvider } from "@/components/theme-provider";
+"use client";
+
+import Background from "@/components/background";
+import Projects from "@/components/projects";
+import Resume from "@/components/resume";
+import Skills from "@/components/skills";
+import { Button } from "@/components/ui/button";
+import home from "@/config/home";
 import { Mail } from "lucide-react";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 h-full flex-col justify-between">
-      <div>
-        <p>
-          <span>hey, i&apos;m abhiroop reddy.</span>
-          <br />
-          <span>i&apos;m a student and a web developer.</span>
-        </p>
+    <div className="flex flex-1 h-full flex-col gap-8 pt-8">
+      <div className="flex flex-col">
+        {home.description.map((line) => (
+          <span key={line}>{line}</span>
+        ))}
       </div>
+      <div className="flex-1 flex flex-col gap-8">
+        <Projects />
+        <Skills />
+        <Resume />
+      </div>
+      <Background />
       <div>
-        <a
-          href="mailto:abhiroopc84@gmail.com"
-          className="flex flex-row items-center gap-2"
-        >
-          <Mail className="h-4 w-4" />
-          contact
-        </a>
+        <Button size={"icon"} variant={"default"} asChild>
+          <a href={home.email} className="flex flex-row items-center gap-2">
+            <Mail className="h-4 w-4" />
+          </a>
+        </Button>
       </div>
     </div>
   );
